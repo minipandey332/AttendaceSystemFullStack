c60568a6858aee4ae46c92c38538e707608049a4
Mini Pandey (NCS) <mini.pandey@in.ncs-i.com>
2024-02-27 14:57:18 +0530

    Complete Backend Code

---
+++ attendance_try2/Migrations/20230912095924_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace attendance_try2.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    month = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    in_time = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    out_time = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    total_hour = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ attendance_try2/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace attendance_try2.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // This attribute specifies auto-increment behavior
+        public int EmployeeId { get; set; }
+
+        
+        public string EmployeeName { get; set; }
+
+        //recent code 
+
+        public string month { get; set; }
+
+        public string Date { get; set; }
+
+        public string in_time { get; set; }
+        public string out_time { get; set; }
+        public string total_hour { get; set; }
+
+
+
+
+
+    }
+}
